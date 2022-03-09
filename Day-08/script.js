14da405037180bf688f59a2166c173a0811d4201
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-09 18:14:40 +0300

    D8 L2 completed

--- Day-08/script.js
+++ Day-08/script.js
@@ -30,3 +30,137 @@ dog.getDogInfo = function () {
 };
 
 console.log(dog.getDogInfo());
+
+//Level 2
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+
+//1
+let keys = Object.keys(users);
+let sLengthArr = [];
+for (let i = 0; i < keys.length; i++) {
+  let s = users[keys[i]].skills;
+  sLengthArr.push(s.length);
+}
+let index = sLengthArr.indexOf(Math.max(...sLengthArr));
+console.log(keys[index]);
+
+//2
+const logins = [];
+const points = [];
+
+for (let [key, values] of Object.entries(users)) {
+  if (values.isLoggedIn === true) {
+    logins.push(key);
+  }
+  if (values.points >= 50) {
+    points.push(key);
+  }
+}
+console.log(logins);
+console.log(points);
+
+//3
+const mern = [];
+for (let [key, values] of Object.entries(users)) {
+  let s = values.skills;
+  switch (true) {
+    case s.includes("MongoDB") &&
+      s.includes("Express") &&
+      s.includes("React") &&
+      s.includes("Node"):
+      mern.push(key);
+      break;
+  }
+}
+console.log(mern);
+
+//4
+const newUsers = Object.assign({}, users);
+newUsers["Jane"] = {};
+
+console.log(newUsers);
+
+//5
+const allKeys = Object.keys(users);
+console.log(allKeys);
+
+// 6
+const allValues = Object.values(users);
+console.log(allValues);
+
+//7
+const countries = {
+  Palestine: {
+    capital: "Jerusalem",
+    populations: 4400000,
+    languages: ["Arabic"],
+  },
+};
+console.log(countries);
