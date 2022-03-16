53c12f8dabd3f552c7b8483b6d0806f259c74100
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-16 14:48:18 +0300

    D11 L1 completed

---
+++ Day-11/script.js
@@ -0,0 +1,64 @@
+//Level 1 
+const constants = [2.72, 3.14, 9.81, 37, 100]
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200,
+  perimeter: 60
+}
+const users = [
+{
+  name:'Brook',
+  scores:75,
+  skills:['HTM', 'CSS', 'JS'],
+  age:16
+},
+{
+  name:'Alex',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'David',
+  scores:75,
+  skills:['HTM', 'CSS'],
+  age:22
+},
+{
+  name:'John',
+  scores:85,
+  skills:['HTML'],
+  age:25
+},
+{
+  name:'Sara',
+  scores:95,
+  skills:['HTM', 'CSS', 'JS'],
+  age: 26
+},
+{
+  name:'Martha',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'Thomas',
+  scores:90,
+  skills:['HTM', 'CSS', 'JS'],
+  age:20
+}
+]
+//1
+let [e, pi, gravity, bodyTemp, boilingTemp] = constants
+console.log(e,pi,gravity, bodyTemp, boilingTemp)
+
+//2
+let [fin, est, sw, den, nor] = countries
+console.log(fin, est, sw, den, nor)
+
+//3
+let { width, height, area, } = rectangle
+console.log(width, height, area)
