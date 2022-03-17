ef613479ccd6f8bfa7a31615ba38458a9dbd5f4b
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-17 12:07:10 +0300

    D12 L1 completed

---
+++ Day-12/script.js
@@ -0,0 +1,22 @@
+//Level 1
+//1
+const textIncome =
+  "He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.";
+const reg = /\d+/g;
+const matches = textIncome.match(reg);
+console.log(matches);
+
+//2
+const sortedPoints = "[-4, -3, -1, -1, 0, 2, 4, 8]";
+const regFirst = sortedPoints.match(/-?\d/);
+const regLast = sortedPoints.match(/(\d+)(?!.*\d)/);
+const distance = Math.abs(parseInt(regFirst) - parseInt(regLast));
+console.log(distance);
+
+//3
+function is_valid_variable(str) {
+  let pattern = /^[a-zA-Z_$]+[0-9]{0,}$/g;
+  return pattern.test(str);
+}
+console.log(is_valid_variable("first_name"));
+console.log(is_valid_variable("first-name"));
