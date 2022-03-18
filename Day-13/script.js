597533d023b6b62803f2a8a4253a43cf1606bf7a
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-18 16:16:33 +0300

    Day 13 completed

--- Day-13/script.js
+++ Day-13/script.js
@@ -23,3 +23,34 @@ console.warn("This is a warning massage");
 
 //3
 console.error("This is an error message");
+
+console.group("Speed Test");
+
+console.group("While Loop");
+console.time("while loop");
+let i = 0;
+while (i < countriesAll.length) {
+  i++;
+}
+console.timeEnd("while loop");
+console.groupEnd();
+
+console.group("For Loop");
+console.time("regular for loop");
+for (let i = 0; i < countriesAll.length; i++) {}
+console.timeEnd("regular for loop");
+console.groupEnd();
+
+console.group("For of Loop");
+console.time("for of loop");
+for (const item of countriesAll) {   
+}
+console.timeEnd("for of loop");
+console.groupEnd();
+
+console.group("forEach Loop");
+console.time("forEach loop");
+countriesAll.forEach((item) => {});
+console.timeEnd("forEach loop");
+console.groupEnd();
+
