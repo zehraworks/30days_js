1e8399e7f6327c8d9be2a14d6b135e4553d61aa4
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-18 16:02:10 +0300

    D13 L1 completed

---
+++ Day-13/script.js
@@ -0,0 +1,17 @@
+//Level 1
+//1
+const countries = [
+  ["Finland", "Helsinki"],
+  ["Sweden", "Stockholm"],
+  ["Norway", "Oslo"],
+];
+console.table(countries);
+
+//2
+console.group("Countries Table")
+console.table(countriesAll);
+console.groupEnd()
+
+//3 console.group applied question 2
+
+
