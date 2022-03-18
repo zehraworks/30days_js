4a610d53806281ac2dddb713299816b59b48224b
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-18 16:07:02 +0300

    D13 L2 completed

--- Day-13/script.js
+++ Day-13/script.js
@@ -8,10 +8,18 @@ const countries = [
 console.table(countries);
 
 //2
-console.group("Countries Table")
+console.group("Countries Table");
 console.table(countriesAll);
-console.groupEnd()
+console.groupEnd();
 
 //3 console.group applied question 2
 
+//Level 2
+//1
+console.assert(10 > 2 * 10, "error");
 
+//2
+console.warn("This is a warning massage");
+
+//3
+console.error("This is an error message");
