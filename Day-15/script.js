21c1fec8f347be3b866a838ecb4a43f21a77f88f
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-19 19:10:09 +0300

    Day 15 completed

--- Day-15/script.js
+++ Day-15/script.js
@@ -18,8 +18,8 @@ class Animal {
   set setNumberOfChildren(numberOfChildren) {
     this.numberOfChildren += numberOfChildren;
   }
-  set setHabitat(habitats) {
-    this.habitats.push(habitats);
+  set setHabitat(habitat) {
+    this.habitats.push(habitat);
   }
   getInfo() {
     return `Name: ${this.name} \nAge: ${this.age} \nColor: ${this.color} \nLegs: ${this.legs}`;
@@ -55,7 +55,7 @@ console.log(cat.getInfo());
 
 // Level 2
 //1
-class Dog extends Animal {
+class Dog extends Cat {
   constructor(name, age, color, legs, numberOfChildren, habitats) {
     super(name, age, color, legs);
     this.numberOfChildren = numberOfChildren;
@@ -65,5 +65,159 @@ class Dog extends Animal {
     return `Name: ${this.name} \nHabitats: ${this.habitats}`;
   }
 }
-let dog = new Dog("Dog", 5, "black", 4, 0, "home");
+let dog = new Dog("Dog", 5, "black", 4, 0, "Garden");
 console.log(dog.getInfo());
+
+// Level 3
+//1
+class Statistics {
+  constructor(data) {
+    this.data = data;
+  }
+  count() {
+    return this.data.length;
+  }
+  sum() {
+    return this.data.reduce((acc, cur) => (acc += cur));
+  }
+  min() {
+    return Math.min(...this.data);
+  }
+  max() {
+    return Math.max(...this.data);
+  }
+  range() {
+    return this.max() - this.min();
+  }
+  mean() {
+    return Math.round(this.sum() / this.count());
+  }
+  median() {
+    let val = Math.round((this.count() - 1) / 2);
+    return this.data.sort((a, b) => a - b)[val];
+  }
+  mode() {
+    const mode = {};
+    let max = 0,
+      count = 0;
+
+    for (let i = 0; i < this.data.length; i++) {
+      const item = this.data[i];
+
+      if (mode[item]) {
+        mode[item]++;
+      } else {
+        mode[item] = 1;
+      }
+
+      if (count < mode[item]) {
+        max = item;
+        count = mode[item];
+      }
+    }
+    return max;
+  }
+  var() {
+    let mean = this.mean();
+
+    return (
+      Math.ceil(
+        (this.data.reduce((acc, cur) => (acc += (cur - mean) ** 2)) /
+          this.count()) *
+          2
+      ) / 2
+    );
+  }
+  std() {
+    return Math.sqrt(this.var()).toFixed(1);
+  }
+  freqDist() {
+    // I dont know how to calculate frequency distribution
+  }
+  describe() {
+    return `Count: ${this.count()}
+Sum: ${this.sum()}
+Min: ${this.min()}
+Max: ${this.max()}
+Range: ${this.range()}
+Mean: ${this.mean()}
+Median: ${this.median()}
+Mode: ${this.mode()}
+Variance: ${this.var()}
+Standard Deviation: ${this.std()}
+Frequency Distribution: ${this.freqDist()}`;
+  }
+}
+
+const ages = [
+  31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37,
+  31, 34, 24, 33, 29, 26,
+];
+const statistics = new Statistics(ages);
+
+console.log(`Count: ${statistics.count()}`);
+console.log(`Sum: ${statistics.sum()}`);
+console.log(`Min: ${statistics.min()}`);
+console.log(`Max: ${statistics.max()}`);
+console.log(`Range: ${statistics.range()}`);
+console.log(`Mean: ${statistics.mean()}`);
+console.log(`Median: ${statistics.median()}`);
+console.log(`Mode: ${statistics.mode()}`);
+console.log(`Variance: ${statistics.var()}`);
+console.log(statistics.describe());
+
+//2
+class PersonAccount {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.incomes = [0];
+    this.expenses = [0];
+  }
+  totalIncome() {
+    let totalIncome = 0;
+
+    for (let income of this.incomes) {
+      totalIncome += income;
+    }
+
+    return totalIncome;
+  }
+  totalExpense() {
+    let totalExpense = 0;
+
+    for (let expense of this.expenses) {
+      totalExpense += expense;
+    }
+
+    return totalExpense;
+  }
+  accountInfo() {
+    return `First Name: ${this.firstName} \nLast Name: ${
+      this.lastName
+    } \nTotal Income: ${this.totalIncome()} \nTotal Expense: ${this.totalExpense()} \nAccount Balance: ${this.accountBalance()}`;
+  }
+  addIncome(amount) {
+    this.incomes.push(amount);
+    return `$${amount} added to incomes`;
+  }
+  addExpense(amount) {
+    this.expenses.push(amount);
+    return `$${amount} added to expenses`;
+  }
+  accountBalance() {
+    let incomes = this.totalIncome(),
+      expenses = this.totalExpense(),
+      balance = incomes - expenses;
+
+    return balance;
+  }
+}
+
+const personAccount = new PersonAccount("Jane", "Doe");
+console.log(personAccount);
+console.log(personAccount.addIncome(5000));
+console.log(personAccount.addIncome(7000));
+console.log(personAccount.addExpense(300));
+console.log(personAccount.addExpense(150));
+console.log(personAccount.accountInfo());
