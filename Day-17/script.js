ba3174531dc5d1a8ff4b710acb40942c64091b15
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-20 14:04:41 +0300

    Day 17 completed

--- Day-17/script.js
+++ Day-17/script.js
@@ -18,3 +18,60 @@ const student = {
 
 const studentJSON = JSON.stringify(student, undefined, 2);
 console.log(localStorage.setItem("student", studentJSON));
+
+// Level 3
+//1
+const personAccount = {
+  firstName: "Jane",
+  lastName: "Doe",
+  incomes: [0],
+  expenses: [0],
+  totalIncome: function () {
+    let totalIncome = 0;
+
+    for (let income of this.incomes) {
+      totalIncome += income;
+    }
+
+    return totalIncome;
+  },
+  totalExpense: function () {
+    let totalExpense = 0;
+
+    for (let expense of this.expenses) {
+      totalExpense += expense;
+    }
+
+    return totalExpense;
+  },
+  accountInfo: function () {
+    return `First Name: ${this.firstName} \nLast Name: ${
+      this.lastName
+    } \nTotal Income: ${this.totalIncome()} \nTotal Expense: ${this.totalExpense()} \nAccount Balance: ${this.accountBalance()}`;
+  },
+  addIncome: function (amount) {
+    this.incomes.push(amount);
+    return `$${amount} added to incomes`;
+  },
+  addExpense: function (amount) {
+    this.expenses.push(amount);
+    return `$${amount} added to expenses`;
+  },
+  accountBalance: function () {
+    let incomes = this.totalIncome(),
+      expenses = this.totalExpense(),
+      balance = incomes - expenses;
+
+    return balance;
+  },
+};
+
+personAccount.addIncome(50);
+personAccount.addIncome(500);
+personAccount.addIncome(230);
+personAccount.addExpense(100);
+personAccount.addExpense(115);
+personAccount.addExpense(15);
+
+const personAccountJSON = JSON.stringify(personAccount, undefined, 4);
+console.log(localStorage.setItem("personAccount", personAccountJSON));
