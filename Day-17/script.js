03d43f4a259e20252568e49d5fe16dd48be3749a
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-20 14:00:59 +0300

    D17 L2 completed

--- Day-17/script.js
+++ Day-17/script.js
@@ -5,3 +5,16 @@ console.log(localStorage.setItem("lastName", "Doe"));
 console.log(localStorage.setItem("age", 30));
 console.log(localStorage.setItem("country", "New Zealand"));
 console.log(localStorage.setItem("city", "Auckland"));
+
+// Level 2
+//1
+const student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 22,
+  isMarried: false,
+  skills: ["HTML", "CSS", "JS", "React", "Node", "PHP"],
+};
+
+const studentJSON = JSON.stringify(student, undefined, 2);
+console.log(localStorage.setItem("student", studentJSON));
