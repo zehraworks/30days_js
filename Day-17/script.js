b3984871ca1bc722d42b3ad66579a997bca17806
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-20 13:57:56 +0300

    D17 L1 completed

---
+++ Day-17/script.js
@@ -0,0 +1,7 @@
+// Level 1
+//1
+console.log(localStorage.setItem("firstName", "Jane"));
+console.log(localStorage.setItem("lastName", "Doe"));
+console.log(localStorage.setItem("age", 30));
+console.log(localStorage.setItem("country", "New Zealand"));
+console.log(localStorage.setItem("city", "Auckland"));
