ab7cbb26dad66699a1af6465ac02976b69045a59
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-25 03:14:22 +0300

    Day 18 completed

--- Day-18/script.js
+++ Day-18/script.js
@@ -18,15 +18,56 @@ fetch(countriesAPI)
   })
   .catch(() => console.log("error"));
 
-  // Level 2
-  //1
-  const fetchData = async () => {
-    try {
-      const response = await fetch(catsAPI)
-      const cats = await response.json()
-      console.log(cats.map(item => item.name))
-    } catch (err) {
-      console.error(err)
-    }
-}
-fetchData()
+// Level 2
+//1
+const fetchData = async () => {
+  try {
+    const response = await fetch(catsAPI);
+    const cats = await response.json();
+    console.log(cats.map((item) => item.name));
+  } catch (err) {
+    console.error(err);
+  }
+};
+fetchData();
+
+// Level 3
+//1
+fetch(catsAPI)
+  .then((response) => response.json())
+  .then((data) => {
+    let average = data.map(
+      (item) =>
+        item.weight.metric
+          .split("-")
+          .reduce((acc, cur) => Number(acc) + Number(cur), 0) / 2
+    );
+    console.log(average);
+  })
+  .catch(() => console.log("error"));
+
+//2
+fetch(countriesAPI)
+  .then((response) => response.json())
+  .then((data) => {
+    let countryWithArea = data
+      .map((item) => [item.name, item.area])
+      .sort((a, b) => {
+        return b[1] - a[1];
+      })
+      .slice(0, 10);
+    console.log(countryWithArea);
+  })
+  .catch(() => console.log("error"));
+
+//3
+fetch(countriesAPI)
+  .then((response) => response.json())
+  .then((data) => {
+    let langArray = data
+      .map((item) => item.languages.map((item) => item.name))
+      .join(",")
+      .split(",");
+    const uniqueLang = new Set(langArray);
+    console.log(uniqueLang.size);
+  });
