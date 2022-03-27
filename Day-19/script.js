b85ecc3aa0962a2c84d828b33a643c21f09a8f2b
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-27 13:03:15 +0300

    D19 L2 completed

--- Day-19/script.js
+++ Day-19/script.js
@@ -1,15 +1,31 @@
 // Level 1
 // 1
 function outerFunction() {
-    let counter = 0
-    function innerFunction() {
-        counter++
-        return counter
-    }
-    return innerFunction
+  let counter = 0;
+  function innerFunction() {
+    counter++;
+    return counter;
+  }
+  return innerFunction;
 }
 
-const result = outerFunction()
-console.log(result())
-console.log(result())
-console.log(result())
+const result = outerFunction();
+console.log(result());
+console.log(result());
+console.log(result());
+
+// level 2
+//1
+function start() {
+  let number = 0;
+  setTimeout(function () {
+    console.log(number++);
+    setTimeout(function () {
+      console.log(number++);
+      setTimeout(function () {
+        console.log(number++);
+      }, 100);
+    }, 200);
+  }, 300);
+}
+start();
