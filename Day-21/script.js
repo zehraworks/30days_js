a64bfe39facfb13325e3b0dc77101782b7893c01
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-03-28 21:43:58 +0300

    D21 L2 completed

--- Day-21/script.js
+++ Day-21/script.js
@@ -27,3 +27,24 @@ console.log(fourthP)
 firstP.setAttribute('class', 'new-class')
 allP[1].setAttribute('class', 'new-class')
 thirdP.classList.add('title', 'new-class')
+
+// Level 2
+//1
+firstP.style.color = 'red'
+secondP.style.backgroundColor = 'blue'
+thirdP.style.fontSize = '24px'
+fourthP.style.border = '1px solid #ccc'
+
+//2
+allP.forEach((title, i) => {
+    if (i % 2 === 0) {
+      title.style.color = 'green'
+    } else {
+      title.style.color = 'red'
+    }
+})
+
+//3
+firstP.innerHTML = '<h4>whats up</h4>'
+secondP.className = 'no-way'
+thirdP.id = 'good-job'
